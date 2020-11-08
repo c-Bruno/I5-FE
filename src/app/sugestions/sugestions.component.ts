0d13ba5b1917de95b4aa7dee67708e8dc00d90e3
c-Bruno <ribeiro.caboclo@gmail.com>
2020-11-08 00:27:03 -0300

    cirado tela de sugestões

--- src/app/sugestions/sugestions.component.ts
+++ src/app/sugestions/sugestions.component.ts
@@ -9,6 +9,15 @@ export class SugestionsComponent implements OnInit {
 
   constructor() { }
 
+  public motivos = [
+    { id: 1, name: 'Consulta' },
+    { id: 2, name: 'Curiosidade' },
+    { id: 3, name: 'Estudo' },
+    { id: 4, name: 'Pesquisa' },
+    { id: 4, name: 'Trabalho' },
+    { id: 4, name: 'Outros' },
+  ];
+
   ngOnInit(): void {
   }
 
